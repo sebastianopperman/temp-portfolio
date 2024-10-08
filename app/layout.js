33a9034400cb22a158bf58f7a261cbce7ba8cbc8
Sebastian Opperman <sebas@Sebastians-MacBook-Pro.local>
2024-10-08 15:45:24 -0700

    Temp page

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import { Metadata } from 'next'
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: 'Sebastian Opperman - Software Engineer',
+  description: 'Sebastian Opperman is a software engineer based in Vancouver. Explore his portfolio and professional experience.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return <main className="w-full h-screen flex flex-col p-10 gap-6">
+    <h1 className="flex flex-col text-7xl md:block leading-[0.8]"><strong>sebastian</strong><span className="opacity-70">opperman</span></h1>
+    <p className="text-xl opacity-80 md:max-w-xl">Hi. I'm <strong className="opacity-100">Sebastian</strong>. I'm a software engineer based in Vancouver. I'm still working on this site, while you wait, check out my profile on <a href="https://www.linkedin.com/in/sebastian-opperman/" target="_blank" className="underline">LinkedIn</a>.</p>
+  </main>;
+}
+
